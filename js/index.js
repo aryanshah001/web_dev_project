f1b32962830894ef17db7dfeba914a9f393299e8
aryan_shah <shaharyan1143@gmail.com>
2026-03-24 21:35:21 +0530

    message

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
-console.log('hello');
-
-console.log('binod')
+const accountName='binod'
+let accountId=1234
+let accountPass='hello@123'
+let accountEmail='hellodefronix@gmail.com'
+console.log(accountName, accountId,accountPass,accountEmail)
