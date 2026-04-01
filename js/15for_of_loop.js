b9737bd74d5443e16e2b0dbfe2d1be4edf1074f2
aryan_shah <shaharyan1143@gmail.com>
2026-04-02 01:52:36 +0530

    message

--- js/15for_of_loop.js
+++ js/15for_of_loop.js
@@ -31,6 +31,7 @@ for (const i of greeting) {
 
 // *************** MAP *************************
 
+// CAN PRINT USING FOR_OF LOOP . (FOR_IN LOOP NOT WORK IN MAP)
 const map = new Map()
 map.set('in', 'India')
 map.set('ch','china')
@@ -47,6 +48,10 @@ for (const [key,value] of map) {
 //OBJECT.
 // WE CANT APPLY FOR_OF LOOP IN OBJECT.  
 // FOR_IN LOOP IS APPLIED IN OBJECT.
+// FOR_EACH LOOP IS MOST COMMONLY USED IN ARRAY.
+// FOR_OF IS ALSO APPLIED IN ARRAY. WE CAN DO WITH FOR_IN ALSO BUT NOT RECOMMENDED.
+
+//USING FOR_IN LOOP IN OBJECT TO PRINT KEYS AND VALUES.
 const myObj = {
     js:'java script',
     cpp:'c++',
@@ -67,3 +72,85 @@ for (const key in myObj) {      // PRINT BOTH KEY AND VALUES
    console.log(` ${key} is shortcut of ${myObj[key]}`);
    
 }
+
+// USING FOR_IN LOOP IN ARRAYS TO PRINT VALUES. (NOT RECOMMENDED)
+const myArr = ['ram','shyam']
+for (const key in myArray) {
+    console.log(myArray[key]);  
+}
+
+//USING FOR_OF LOOP TO PRINT ARRAYS . (RECOMMENDED BUT LESS COMMON)
+const arr = ['ram','sit']
+for (const i of arr) {
+    console.log(i);   
+}
+
+//USING FOR_EACH LOOP TO PRINT ARRAY. (MOST COMMON AND RECOMMENDED)
+const coding = ['js', 'c++' , 'python', 'ruby']
+
+coding.forEach(  function (i){
+    console.log(i);
+    
+}   )
+
+//PRINT USING ARROW FUNCTION IN FOR_EACH LOOP.
+const coding = ['js', 'python']
+coding.forEach(  (i) => {
+    console.log(i);
+    
+}   )
+
+
+//CAN PRINT DECLARING FUNCTION ALSO.(LESS USED)
+const coding = ['ram', 'shyam']
+function printMe(i){
+    console.log(i);  
+}
+coding.forEach(printMe)
+
+
+// MORE ABOUT FOR_EACH.
+const fruit = ['apple','mango','banana']
+fruit.forEach(  (i , index, array) => {
+    console.log(i , index , array);
+    
+} )
+
+
+// MORE OBJECT INSIDE ARRAY [{}, {}, {}]
+
+const myCoding = [
+    {
+        languageName:'javascript',
+        languageFile:'js'
+    },
+    
+    {
+        languageName:'python',
+        languageFile:'py'
+    },
+    {
+        languageName:'java',
+        languageFile:'java'
+    },
+
+]
+
+//print in  diff ways (ist)
+myCoding.forEach(    (i) => {
+    console.log(i.languageName);
+    console.log(i.languageFile);
+    console.log(); 
+}   )
+
+//print in  diff ways (2nd)
+myCoding.forEach(  (i) => {
+    console.log(i.languageName);  
+}  )
+console.log();
+myCoding.forEach(  (i) => {
+    console.log(i.languageFile);
+    
+}  )
+
+
--- js/practice.js
+++ js/practice.js
@@ -1,24 +1,5 @@
-const myObj = {
-    js:'java script',
-    cpp:'c++',
-    hw:'hello world'
-}
-
-// PRINT ONLY KEYS.
-for (const key in myObj) {
-   console.log(key);
+const myarr = ['ram','shyam','sita']
+for (const i of myarr) {
+   console.log(i);
    
 }
-
-// PRINT ONLY VALUES.
-for (const key in myObj) {
-   console.log(myObj[key]);
-   
-}
-
-// PRINT BOTH KEY AND VALUES
-for (const key in myObj) {
-   console.log(` ${key} is shortcut of ${myObj[key]}`);
-   
-}
-
