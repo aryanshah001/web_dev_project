e1e18baa770abd1d75933f49ba7f17c4662a1c0f
aryan_shah <shaharyan1143@gmail.com>
2026-06-03 00:33:27 +0530

    message

---
+++ react/src/project_router_3/src/components/index.js
@@ -0,0 +1,6 @@
+export {default as Layout} from '../Layout'
+export{default as Home} from './Home/Home'
+export {default as Contact} from './Contact/Contact'
+export {default as About} from './About/About'
+export {default as User} from './User/User'
+export {default as Github} from './Github/Github'
