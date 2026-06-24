9d1994d842acfffd7c54bee56f02cb4c91ff056e
aryan_shah <shaharyan1143@gmail.com>
2026-06-25 00:10:33 +0530

    message

--- react/src/proj_7_context_ls/src/App.jsx
+++ react/src/proj_7_context_ls/src/App.jsx
@@ -1,29 +1,39 @@
-import { useState } from "react";
-import { TodoProvider } from "./context/TodoContext";
+import { useEffect, useState } from "react";
+import { TodoProvider } from "./context";
 
 function App() {
-  const [todos, setTodos] = useState([]);
 
-  const addTodo = (todo) => {
-        setTodos((prev) => [{id:Date.now() , ...todo}, ...prev])
-  }
+    const [todos , setTodos] = useState([])
 
-  const updateTodo = (id , todo) => {
-      setTodos((prev) => prev.map((prevtodo) => prevtodo.id ===id ? todo : prevtodo))
-  }
+    const addTodo = (todo) => {
+        setTodos((prev) => [{id:Date.now() ,...todo},...prev])
+    }
 
-  const deleteTodo  = (id) => {
-      setTodos((prev) => prev.filter((todo) => todo.id !== id))
-  }
+    const updateTodo = (id , todo) => {
+        setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? todo : prevTodo))
+    }
 
-  const toggleComplete = (id) => {
-      setTodos((prev) => prev.map((prevtodo) => prevtodo.id === id  ? {...prevtodo , completed:!prevtodo.completed} : prevtodo ))
-  }
+    const deleteTodo = (id) => {
+        setTodos((todo) => todo.filter((delTodo) => delTodo.id !== id))
+    }
 
+    const toggleComplete = (id) => {
+        setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, completed:!prevTodo.completed} : prevTodo ))
+    }
+
+    useEffect(() => {
+        const todos = JSON.parse(localStorage.getItem('todos'))
+
+        if(todos && todos.length > 0){
+            setTodos(todos)
+        }
+    } , [])
+
+    useEffect(() => {
+        localStorage.setItem('todos' , JSON.stringify(todos))
+    } , [todos])
   return (
-    <TodoProvider
-      value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
-    >
+    <TodoProvider value={{todos , addTodo , updateTodo , deleteTodo , toggleComplete}} >
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
