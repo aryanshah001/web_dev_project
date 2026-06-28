99f936668a2f0414eabb6d8820c4cf45a29b0607
aryan_shah <shaharyan1143@gmail.com>
2026-06-29 00:53:25 +0530

    message

--- react/src/proj_7_context_ls/src/App.jsx
+++ react/src/proj_7_context_ls/src/App.jsx
@@ -1,36 +1,38 @@
 import { useEffect, useState } from "react";
 import { TodoProvider } from "./context";
+import { TodoForm } from "./components/TodoForm";
+import { TodoItem } from "./components/TodoItem";
 
 function App() {
 
     const [todos , setTodos] = useState([])
 
     const addTodo = (todo) => {
-        setTodos((prev) => [{id:Date.now() ,...todo},...prev])
+        setTodos((prev) => [{id:Date.now() ,...todo},...prev])  // MAKES COMPLETELY NEW ARRAY
     }
 
     const updateTodo = (id , todo) => {
-        setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? todo : prevTodo))
+        setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? todo : prevTodo)) //REMOVE OLD AND MAKE COMPLETELY NEW ARRAY
     }
 
     const deleteTodo = (id) => {
-        setTodos((todo) => todo.filter((delTodo) => delTodo.id !== id))
+        setTodos((todo) => todo.filter((delTodo) => delTodo.id !== id))  //FILTER OUT 
     }
 
     const toggleComplete = (id) => {
-        setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, completed:!prevTodo.completed} : prevTodo ))
+        setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, completed:!prevTodo.completed} : prevTodo ))     //REMOVE AND ADD ONLY ONE VALUE
     }
 
     useEffect(() => {
-        const todos = JSON.parse(localStorage.getItem('todos'))
+        const storedTodo = JSON.parse(localStorage.getItem('todo1'))  //Here name inside getItem(a) i.e = a, must be same to name in setItem(a) 
 
-        if(todos && todos.length > 0){
-            setTodos(todos)
+        if(storedTodo && storedTodo.length > 0){
+            setTodos(storedTodo)
         }
     } , [])
 
     useEffect(() => {
-        localStorage.setItem('todos' , JSON.stringify(todos))
+        localStorage.setItem('todo1' , JSON.stringify(todos))   //todos in stringify comes from state const [todos, setTodos]
     } , [todos])
   return (
     <TodoProvider value={{todos , addTodo , updateTodo , deleteTodo , toggleComplete}} >
@@ -39,9 +41,19 @@ function App() {
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
-          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+            <TodoForm />
+            </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id}
+              className="w-full"
+              >
+                <TodoItem  todo={todo}/>
+              </div>
+            ))}
           </div>
         </div>
       </div>
---
+++ react/src/proj_7_context_ls/src/components/index.js
@@ -0,0 +1,11 @@
+import {TodoForm} from "./TodoForm";
+import {TodoItem} from "./TodoItem";
+
+export {TodoForm , TodoItem}
+
+
+
+// OR WE CAN ALSO ONLY EXPORT WITHOUT IMPORTING.
+
+// export {TodoForm} from './TodoForm';
+// export {TodoItem} from './TodoItem';
